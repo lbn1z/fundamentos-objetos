003141301d6759fbf8e2db4b80c611db55188964
Leonardo <leobrustolin1@gmail.com>
2024-12-08 10:18:25 -0300

    objs

---
+++ objetos-1/dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+    nome: 'Joao Silva',
+    idade: 32,
+    cpf: '3213215425',
+    turma: 'JavaScript',
+    bolsista: true, 
+    telefone: ['551199999999', '551199999998']
+}
+
+console.log(estudante.telefone[0]);
---
+++ objetos-1/objs-aninhados.js
@@ -0,0 +1,17 @@
+const estudante = {
+    nome: 'Joao Silva',
+    idade: 32,
+    cpf: '3213215425',
+    turma: 'JavaScript',
+    bolsista: true, 
+    telefone: ['551199999999', '551199999998']
+}
+
+
+estudante.endereco = {
+    rua: 'Rua São José',
+    numero: '0',
+    complemento: 'apto 1'
+}
+
+console.log(estudante.endereco.rua);
