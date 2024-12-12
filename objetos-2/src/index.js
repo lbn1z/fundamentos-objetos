ce955df23c4aed2ba5675e2f9275621e8ca1f713
Leonardo <leobrustolin1@gmail.com>
2024-12-12 18:23:24 -0300

    logica-projeto

--- objetos-2/src/index.js
+++ objetos-2/src/index.js
@@ -3,9 +3,35 @@ const fs = require('fs');
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
+// node src/index.js arquivos/texto-web.txt em gitbash
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    console.log(texto);
+    quebraEmParagrafos(texto);
+    // verificaPalavrasDuplicadas(texto);
 })
 
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if (!paragrafo) return [];
+        return verificaPalavrasDuplicadas(paragrafo);
+    })
+    console.log(contagem);
+}
 
+function limpaPalavras(palavra) {
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach(palavra => {
+        if (palavra.length >= 3) {
+            const palavraLimpa = limpaPalavras(palavra);
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) +1
+        }
+    })
+    return resultado;   
+}
 
